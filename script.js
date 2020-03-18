5c3d0fadbac6184838631186cb9c2d70184c3f1c
benmansour <naoufelbenmensour@gmail.com>
2020-03-19 00:59:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,37 @@ function addDepa(){
 	option = $('#depa').val();
 	$('#departement').append('<option>' +option+ '</option>');
 }
+jQuery(document).ready(function($) {
+
+	// $(".headroom").headroom({
+	// 	"tolerance": 20,
+	// 	"offset": 50,
+	// 	"classes": {
+	// 		"initial": "animated",
+	// 		"pinned": "slideDown",
+	// 		"unpinned": "slideUp"
+	// 	}
+	// });
+	$('#login').hide();
+	$('#register').hide();
+
+
+
+	$('#connexion').click(function(){
+		$('#login').show();
+		$('#home').hide();
+		$('#contact').hide();
+	});
+
+	$('#regi-1').click(function(){
+		$('#register').show();
+		$('#login').hide();
+		$('#home').hide();
+	});
+
+	$('.btn1').click(function(){
+		$('#login').show();
+		$('#home').hide();
+		$('#register').hide();
+	});
+});
